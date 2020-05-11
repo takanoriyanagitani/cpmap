6fdc0877c000311c401b850207dbdc437ab57ade
yanagitani <takanori.yanagitani@gmail.com>
2020-05-11 15:04:46 +0900

    longer timeout

--- index.chrome_test.js
+++ index.chrome_test.js
@@ -2,6 +2,8 @@ const { Builder } = require("selenium-webdriver")
 
 const c = require("selenium-webdriver/chrome")
 
+const timeout = 32768
+
 require("chromedriver")
 
 describe("chrome", () => {
@@ -13,31 +15,31 @@ describe("chrome", () => {
     .then(b=>b.setChromeOptions(new c.Options().headless()))
     .then(b=>b.build())
     .then(b=>Object.assign(state, {browser:b}))
-  , 16384)
+  , timeout)
 
   afterAll(() => Promise.resolve(state.browser)
     .then(b=>b.quit())
-  , 16384)
+  , timeout)
 
   test("str2chars", () => Promise.resolve(state.browser)
     .then(b=>b.get("https://takanoriyanagitani.github.io/cpmap/docs/tests/index/s2c.html"))
     .then(_=>state.browser)
     .then(b=>b.getTitle())
     .then(t=>expect(t == "OK").toBe(true))
-  , 16384)
+  , timeout)
 
   test("str2pairs", () => Promise.resolve(state.browser)
     .then(b=>b.get("https://takanoriyanagitani.github.io/cpmap/docs/tests/index/s2p.html"))
     .then(_=>state.browser)
     .then(b=>b.getTitle())
     .then(t=>expect(t == "OK").toBe(true))
-  , 16384)
+  , timeout)
 
   test("str2map", () => Promise.resolve(state.browser)
     .then(b=>b.get("https://takanoriyanagitani.github.io/cpmap/docs/tests/index/s2m.html"))
     .then(_=>state.browser)
     .then(b=>b.getTitle())
     .then(t=>expect(t == "OK").toBe(true))
-  , 16384)
+  , timeout)
 
 })
